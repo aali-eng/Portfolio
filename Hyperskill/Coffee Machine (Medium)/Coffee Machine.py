ab16387c58782d8a8a1aa163266162c4a231c16c
aali-eng <66701535+aali-eng@users.noreply.github.com>
2025-10-21 13:54:16 +0300

    Update Coffee Machine.py

--- Hyperskill/Coffee Machine (Medium)/Coffee Machine.py
+++ Hyperskill/Coffee Machine (Medium)/Coffee Machine.py
@@ -99,44 +99,42 @@ def calculate_fill(water, milk, beans, cups):
         "money": supply["money"],
     }
 
-class Coffee_Machine:
-    def __init__(self, var):
-        self.var = input()
-    def operations(self):
-        while True:
-            action = input("Write action (buy, fill, take, remaining, exit): ")
-            if action == "buy":
-                option = input(
-                    "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
-                if option == "1":
-                    calculate_buy("1")
-                elif option == "2":
-                    calculate_buy("2")
-                elif option == "3":
-                    calculate_buy("3")
-                elif option == "back":
-                    operations()
-                    break
-            elif action == "fill":
-                calculate_fill(
-                    int(input("Write how many ml of water you want to add: ")),
-                    int(input("Write how many ml of milk you want to add: ")),
-                    int(input("Write how many grams of coffee beans you want to add: ")),
-                    int(input("Write how many disposable cups you want to add: ")))
-            elif action == "take":
-                cash = supply["money"]
-                print(f"I gave you ${cash}")
-                supply["money"] = 0
 
-            elif action == "remaining":
-
-                show_supply()
-
-            elif action == "exit":
-
+def operations():
+    while True:
+        action = input("Write action (buy, fill, take, remaining, exit): ")
+        if action == "buy":
+            option = input(
+                "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
+            if option == "1":
+                calculate_buy("1")
+            elif option == "2":
+                calculate_buy("2")
+            elif option == "3":
+                calculate_buy("3")
+            elif option == "back":
+                operations()
                 break
+        elif action == "fill":
+            calculate_fill(
+                int(input("Write how many ml of water you want to add: ")),
+                int(input("Write how many ml of milk you want to add: ")),
+                int(input("Write how many grams of coffee beans you want to add: ")),
+                int(input("Write how many disposable cups you want to add: ")))
+        elif action == "take":
+            cash = supply["money"]
+            print(f"I gave you ${cash}")
+            supply["money"] = 0
+
+        elif action == "remaining":
+
+            show_supply()
+
+        elif action == "exit":
+
+            break
 
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
 
-        operations()
+    operations()
